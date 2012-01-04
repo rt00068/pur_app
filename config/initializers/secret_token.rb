f0a2081fa5ea30642e198d586d9d1a7e338730f5
Your Name <rt00068@surrey.ac.uk>
2012-01-04 19:09:34 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PurApp::Application.config.secret_token = 'f6134ebc1add399f6454302762557459b920838682dbdd08ec2380f06e712cbd52533c1486608418dade59f4339bf7e6bddf92b9af9e11233c49a17ce64e70ca'
